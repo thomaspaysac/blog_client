f225a3809fde8583b609b380124fd36614e8a8ef
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-02 15:57:56 +0200

    Add post comment function

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -1,3 +1,6 @@
+import { useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
 const IndividualComment = ({ comment }) => {
   return (
     <div className="individual-comment_container">
@@ -9,6 +12,24 @@ const IndividualComment = ({ comment }) => {
 }
 
 const ArticleComments = ({ comments }) => {
+  const { id } = useParams();
+  const navigateTo = useNavigate();
+  
+  const submitComment = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById('comment_form');
+    const data = {};
+    new FormData(form).forEach((value, key) => data[key] = value);
+    await fetch(`https://blogapi-production-5dee.up.railway.app/posts/${id}/comment`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    });
+    navigateTo(`/post/${id}`);
+  }
+
   if (!comments.length) {
     return (
       <div>
@@ -17,15 +38,25 @@ const ArticleComments = ({ comments }) => {
     )
   } else {
     return (
-      <div className="comments_container">
-        {
-          comments.map((el) => {
-            return (
-              <IndividualComment key={el._id} comment={el} />
-            )
-          })
-        }
-      </div>
+      <>
+        <div>New comment:
+        <form id='comment_form' onSubmit={submitComment}>
+          <textarea id='text' name='text'></textarea>
+          <button type='submit'>Submit comment</button>
+        </form>
+          
+        </div>
+        <div className="comments_container">
+          {
+            comments.map((el) => {
+              return (
+                <IndividualComment key={el._id} comment={el} />
+              )
+            })
+          }
+        </div>
+      </>
+      
     )
   }
 }
