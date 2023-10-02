c26d07a74f0ff45c82082e58281dcfe4988e64ba
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-02 21:23:03 +0200

    Add login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import Header from "../components/Header"
 
 export const Login = () => {
+  const navigateTo = useNavigate();
+
+  const login = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById('login_form');
+    const data = {};
+    new FormData(form).forEach((value, key) => data[key] = value);
+    await fetch(`https://blogapi-production-5dee.up.railway.app/users/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    });
+    navigateTo('/');
+  }
+
   return (
     <div>
       <Header />
       <h2>Log in</h2>
-      <form id='login_form'>
+      <form id='login_form' onSubmit={login}>
         <div>
           <label htmlFor="username">Username: </label>
           <input type='text' id='username' name='username' />
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,6 +5,20 @@ export const Signup = () => {
     <div>
       <Header />
       <h2>Sign up</h2>
+      <form id='signup_form'>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input type='text' id='username' name='username' />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input type='password' id='password' name='password' />
+        </div>
+        <div>
+          <label htmlFor="password_confirm">Confirm password: </label>
+          <input type='password' id ='password_confirm' name='password confirm' />
+        </div>
+      </form>
     </div>
   )
 }
